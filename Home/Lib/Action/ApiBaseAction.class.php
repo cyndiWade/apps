413b0bc1152d23ed7dbee9115da53eabd6053873
cyndiWade <zhanglin492103904@qq.com>
2013-07-18 15:35:50 +0800

    wade

--- Home/Lib/Action/ApiBaseAction.class.php
+++ Home/Lib/Action/ApiBaseAction.class.php
@@ -24,7 +24,7 @@ class ApiBaseAction extends Action {
 		}  else {
 			$this->oApp = D('App')->getApp(getRequest('company'));	
 		}
-		$this->oApp->id = 1;		//公司id
+		//$this->oApp->id = 1;		//公司id
 		
 		
 		if (empty($this->oApp)) {
@@ -39,7 +39,6 @@ class ApiBaseAction extends Action {
 	
 	/*
 	 * 回调函数
-	 * 
 	 */
 	protected function callback($status = STATUS_SUCCESS, $msg = '获取成功！', $data = array()) {
 		$ret = array(
